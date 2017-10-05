38b8a8172eb0c372431fcb3aed4aa1c691959261
Jorge Garcia <jorge.gcia.gcia@gmail.com>
2017-10-05 14:01:05 +0200

    First Version

---
+++ CentralServerConceptTest/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using CentralServerConceptTest.Bussiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CentralServerConceptTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public async Task<string> TestStatus()
+        {
+            CustomHttpClient client = new CustomHttpClient(true);
+            return await client.InvokeGet("localhost:8080", "api/Action/Status");
+        }
+    }
+}
---
+++ CentralServerConceptTest/Startup.cs
@@ -0,0 +1,16 @@
+﻿using CentralServerConceptTest.Bussiness;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CentralServerConceptTest.Startup))]
+namespace CentralServerConceptTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            CustomHttpClient.SetServerCertificateValidation("CN=DESKTOP-L1FD9CA.NA.LOCAL",@"c:\work\client.cer");
+            ConfigureAuth(app);
+        }
+    }
+}
